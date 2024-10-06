9fe10a34565855b7c5099a3e3c8db7dc43899746
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-06 13:56:26 +0530

    Initial commit

---
+++ src/main/java/com/example/NoteTakerV2/NoteTakerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.NoteTakerV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NoteTakerV2/NoteTakerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoteTakerV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
