7ffcf7ead331f5f7f57d0908ee45f124dff30311
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-10-06 14:05:41 +0530

    DAO Error solve

--- src/main/java/com/example/NoteTakerV2/dao/UserDao.java
+++ src/main/java/com/example/NoteTakerV2/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.example.NoteTakerV2.dao;
 
+import com.example.NoteTakerV2.entity.UserEntity;
 import org.example.notetaker.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
